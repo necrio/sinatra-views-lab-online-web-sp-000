38247d1b8840c3d2e61e040832af312b002459a7
Keith Isham <kisham8899@gmail.com>
2019-08-22 00:50:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 class App < Sinatra::Base
 
-	get '/' do
+	get '/hello' do
 		erb :hello
 	end
 	
-		get '/' do
+		get '/goodbye' do
 		erb :goodbye
 	end
 
-	get '/' do
+	get '/date' do
 		erb :date
 	end
 
